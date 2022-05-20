3efce6036fea157156f69140574975d535d1047d
daft2 <daftdevs@gmail.com>
2022-05-20 09:58:14 +0700

    ref: hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.1",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${METAMASK_PRIVATE_KEY}`]
+    }
+  },
 };
