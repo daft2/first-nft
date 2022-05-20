04830361b45c03e3427168c6016b96543332c6f7
daft2 <daftdevs@gmail.com>
2022-05-20 09:52:18 +0700

    init

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,32 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Contains the implementation of the ERC712 standard to be inherit for our NFT
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// Provides counters to keep track of the total number of NFTs minted and set the Unique ID
+import "@openzeppelin/contracts/utils/Counters.sol";
+// Sets up access control so only the owner of the smart contract can mint NFTs
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MyNFT", "NFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
